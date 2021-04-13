3a445f3c32eabf6ed85728876677fb4061ae9d85
Josh <josh.vigar@thebeansgroup.com>
2021-04-13 09:54:16 +0100

    Add validations to review
    Adds rating validations for presence, numericality and inclusion. Adds
    username validations for presence and length.

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
 class Review < ApplicationRecord
-  belongs_to :book
+  belongs_to :book, validate: true
+
+  validates :rating, :user_name, presence: true
+  validates :rating,
+            numericality: { only_integer: true },
+            inclusion: { in: 0..5 }
+  validates :user_name, length: { minimum: 3 }
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -4,4 +4,11 @@ require 'rails_helper'
 
 RSpec.describe Review, type: :model do
   it { is_expected.to belong_to(:book) }
+
+  it { is_expected.to validate_presence_of(:rating) }
+  it { is_expected.to validate_numericality_of(:rating).only_integer }
+  it { is_expected.to validate_inclusion_of(:rating).in_range(0..5) }
+
+  it { is_expected.to validate_presence_of(:user_name) }
+  it { is_expected.to validate_length_of(:user_name).is_at_least(3) }
 end
